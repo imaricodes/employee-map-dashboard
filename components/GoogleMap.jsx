fad74211255ac25013fcac37cfe1766264bdf1eb
Imari Childress <imaricodes@gmail.com>
2024-07-03 12:43:09 -0400

    add dashboardContext

--- components/GoogleMap.jsx
+++ components/GoogleMap.jsx
@@ -2,44 +2,48 @@
 
 import React, { useState, useCallback, useMemo, useEffect } from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { useDashboardContext } from "@/contexts/DashboardContext";
 
 const containerStyle = {
-    width: '100%', // Set width to 100% for responsiveness
-    height: '100%', // Set height to 100% for responsiveness
-    // width: '400px', // Set width to 100% for responsiveness
-    // height: '400px', // Set height to 100% for responsiveness
-  };
+  width: "100%", // Set width to 100% for responsiveness
+  height: "100%", // Set height to 100% for responsiveness
+  // width: '400px', // Set width to 100% for responsiveness
+  // height: '400px', // Set height to 100% for responsiveness
+};
 
 const center = {
   lat: -3.745,
   lng: -38.523,
 };
 
-const users = [
-  {
-    userId: 100,
-    location: {
-      lat: -3.745,
-      lng: -38.523,
-    },
-  },
-  {
-    userId: 200,
-    location: {
-        lat: -3.745 + 0.001, // Slightly adjust latitude
-        lng: -38.523,
-    },
-  },
-  {
-    userId: 200,
-    location: {
-        lat: -3.745,
-        lng: -38.523 - 0.001,
-    },
-  },
-];
+// const users = [
+//   {
+//     userId: 100,
+//     location: {
+//       lat: -3.745,
+//       lng: -38.523,
+//     },
+//   },
+//   {
+//     userId: 200,
+//     location: {
+//       lat: -3.745 + 0.001, // Slightly adjust latitude
+//       lng: -38.523,
+//     },
+//   },
+//   {
+//     userId: 200,
+//     location: {
+//       lat: -3.745,
+//       lng: -38.523 - 0.001,
+//     },
+//   },
+// ];
 
 function MyComponent() {
+  const { users } = useDashboardContext();
+  console.log('users in google map module', users);
+
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -48,21 +52,18 @@ function MyComponent() {
 
   const [map, setMap] = useState(null);
 
-  const onLoad = useCallback(
-    function callback(map) {
-      // This is just an example of getting and using the map instance!!! don't just blindly copy!
-      const bounds = new window.google.maps.LatLngBounds(center);
+  const onLoad = useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
 
-      users.forEach((location) => {
-        bounds.extend({ lat: location.location.lat, lng: location.location.lng });
-      });
+    users.forEach((location) => {
+      bounds.extend({ lat: location.location.lat, lng: location.location.lng });
+    });
 
-      map.fitBounds(bounds);
+    map.fitBounds(bounds);
 
-      setMap(map);
-    },
-    []
-  );
+    setMap(map);
+  }, []);
 
   const onUnmount = useCallback(function callback(map) {
     setMap(null);
@@ -79,8 +80,7 @@ function MyComponent() {
       {/* Child components, such as markers, info windows, etc. */}
       <>
         {users.map((marker, index) => (
-
-            //marker.location is an object that has lat and lng as properties
+          //marker.location is an object that has lat and lng as properties
           <Marker key={index} position={marker.location} />
         ))}
       </>
---
+++ contexts/DashboardContext.jsx
@@ -0,0 +1,87 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useState,
+  useCallback,
+} from "react";
+
+
+const usersData = [
+    {
+      userId: 100,
+      location: {
+        lat: -3.745,
+        lng: -38.523,
+      },
+    },
+    {
+      userId: 200,
+      location: {
+          lat: -3.745 + 0.001, // Slightly adjust latitude
+          lng: -38.523,
+      },
+    },
+    {
+      userId: 200,
+      location: {
+          lat: -3.745,
+          lng: -38.523 - 0.001,
+      },
+    },
+  ];
+
+export const DashboardContext = createContext(null);
+
+export default function DashboardContextProvider({ children }) {
+  const [users, setUsers] = useState(usersData);
+
+
+  /*
+  These setters will update the state and also update the indexedDB. There one for each state except for triggerMapReDraw.
+  triggerMapReDraw is used to force a re-render of the map when the map view is changed. It is not stored in indexedDB
+
+  the useCallback hook is used to memoize the setters so that they are not re-created on each render, because they are custom setters they are not automatically memoized by react
+  */
+
+  const usersSetter = useCallback((data) => {
+    setUsers(data);
+  }, []);
+
+
+  return (
+    <DashboardContext.Provider
+      value={{
+        users: users,
+        usersSetter: usersSetter
+      }}
+    >
+      {children}
+    </DashboardContext.Provider>
+  );
+}
+
+/*
+
+This approach exports the context as a custom hook.
+An advantage of this approach is that it reduces the amount of code
+needed to use the global context in a component.
+This approach also handles the (error) case where a component tries to use
+the global context outside of a DashboardContextProvider.
+
+Usage (in client compoent only):
+import { useDashboardContext } from "@/contexts/DashboardContext";
+const {dashboardState, setDashboardState} = useDashboardContext();
+
+*/
+
+export function useDashboardContext() {
+  const context = useContext(DashboardContext);
+  if (!context) {
+    throw new Error(
+      "useDashboardContext must be used within a DashboardContextProvider",
+    );
+  }
+  return context;
+}
